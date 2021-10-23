b442db51534a5a7c2cb725f90d0fbfc8e351ce30
DerZwergGimli <github-gai-22>
2021-10-23 16:00:26 +0200

    Files created

---
+++ influx_db_helper.py
@@ -0,0 +1,44 @@
+import socket
+
+import requests.exceptions
+import urllib3.exceptions
+
+import file_helper
+from influxdb import InfluxDBClient
+import influx_templates
+from loguru import logger
+
+
+def write_viessmann_data_to_influx_db(inlfux_db_file_path: str, json_viessmann_data):
+    json_influx = file_helper.read_file_to_json(inlfux_db_file_path)
+    b_write_to_db = False
+    try:
+        client = InfluxDBClient(json_influx["credentials"]["address"],
+                                json_influx["credentials"]["port"],
+                                json_influx["credentials"]["user"],
+                                json_influx["credentials"]["password"],
+                                json_influx["credentials"]["database_name"],
+                                timeout=1)
+
+        client.create_database(json_influx["credentials"]["database_name"])
+        b_write_to_db = True
+    except (socket.timeout, urllib3.exceptions.ConnectTimeoutError, requests.exceptions.ConnectTimeout):
+        logger.error("Timeout when trying to connect to InfluxDB")
+    except requests.exceptions.ConnectionError:
+        logger.error("ConnectionError when trying to connect to InfluxDB")
+
+    if b_write_to_db:
+        for data_point in json_viessmann_data['data']:
+            value = 0.0
+            unit = "empty"
+            status = "none"
+            if data_point.get('properties').get('value'):
+                value = data_point.get('properties').get('value').get('value')
+            if data_point.get('properties').get('unit'):
+                unit = data_point.get('properties').get('unit').get('value')
+            if data_point.get('properties').get('status'):
+                status = data_point.get('properties').get('status').get('value')
+            json_database_body = influx_templates.json_influx_template(data_point.get('feature'), data_point.get('timestamp'), data_point.get('deviceId'), value)
+            client.write_points(json_database_body)
+            print(json_database_body)
+
---
+++ main.py
@@ -0,0 +1,72 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#GLOBALS
+import json
+import re
+
+import influx_templates
+import pandas
+import requests
+from requests.structures import CaseInsensitiveDict
+import pandas as pd
+from influxdb import InfluxDBClient
+import viessmann_helper
+import influx_db_helper
+from loguru import logger
+
+viessmann_api_file_path = "viessmann_api.json"
+inlfux_db_file_path = "influx_db.json"
+
+
+
+def menue():
+
+    print("============================================")
+    print("Welcome to the Vissman API extractor")
+    print("1 = CreateToken")
+    print("2 = GetToken")
+    print("3 = UpdateToken")
+    print("4 = installation_id")
+    print("5 = get_gateway_serial")
+    print("6 = get_device_id")
+    print("7 = get_features_list_all")
+    print("8 = get_features_form_list_by_device")
+    print("9 = write_viessmann_data_to_influx_db")
+    print("0 = exit")
+    print("============================================")
+    case = input("Enter NUMBER:")
+
+    if case == "1":
+        viessmann_helper.token_authorize(viessmann_api_file_path)
+    elif case == "2":
+        viessmann_helper.get_token(viessmann_api_file_path)
+    elif case == "3":
+        viessmann_helper.get_update_token(viessmann_api_file_path)
+    elif case == "4":
+        viessmann_helper.installation_id(viessmann_api_file_path)
+    elif case == "5":
+        viessmann_helper.get_gateway_serial(viessmann_api_file_path)
+    elif case == "6":
+        viessmann_helper.get_device_id(viessmann_api_file_path)
+    elif case == "7":
+        viessmann_helper.get_features_list_all(viessmann_api_file_path)
+    elif case == "8":
+        viessmann_helper.get_features_form_list_by_device(viessmann_api_file_path, "0")
+    elif case == "9":
+        influx_db_helper.write_viessmann_data_to_influx_db(inlfux_db_file_path, viessmann_helper.get_features_form_list_by_device(viessmann_api_file_path, "0"))
+    elif case == "0":
+        quit()
+    menue()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    logger.add("log_file.log", rotation="10 MB", colorize=True, format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}")
+
+    logger.info("TESTLog entry")
+
+    menue()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
