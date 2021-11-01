0668670d5c379167096e0b32f23d558d8f684c0a
DerZwergGimli <paule22@protonmail.com>
2021-11-01 13:09:51 +0100

    refiend time of status influx write

--- influx_helper/influx_db_helper.py
+++ influx_helper/influx_db_helper.py
@@ -1,3 +1,4 @@
+import datetime
 import socket
 import time
 
@@ -13,7 +14,7 @@ from loguru import logger
 
 def write_viessmann_data_to_influx_db(inlfux_db_file_path: str, json_viessmann_data):
     json_influx = file_helper.read_file_to_json(inlfux_db_file_path)
-    b_write_to_db = False
+    b_write_to_db = True
     try:
         client = InfluxDBClient(json_influx["credentials"]["address"],
                                 json_influx["credentials"]["port"],
@@ -79,7 +80,7 @@ def write_viessmann_data_to_influx_db(inlfux_db_file_path: str, json_viessmann_d
                       "viErrorId": json_viessmann_data.get("viErrorId")}
             json_database_body = influx_templates.json_influx_template_modular(
                 measurement="api.status",
-                time=time.time(),
+                time=datetime.datetime.now(),
                 tags=tags,
                 fields=fields
             )
